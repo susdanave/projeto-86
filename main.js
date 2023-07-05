1301dbb18999f96a095246de96e222dd2c824244
susdanave <121106074+susdanave@users.noreply.github.com>
2023-07-05 20:23:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function newImage()
+{
+ fabric.Image.fromURL'BirthdayImage.jpg', function(Img) {
+    blockImageObject = Img
+ }	
+}
+
+function playSound(){
+	
+}
